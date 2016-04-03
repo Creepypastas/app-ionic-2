f94de81acbc1f276bbff919dc029fc5754dfc56c
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2016-04-02 18:19:51 -0600

    Guardo y cargo las categorías en localStorage

--- app/pages/categorias/categorias.js
+++ app/pages/categorias/categorias.js
@@ -14,23 +14,37 @@ export class CategoriasPage {
 
   constructor(nav, navParams, http) {
     this.nav = nav;
-    this.creepypastasCategorias = [];
 
+    this.creepypastasCategoriasMap = JSON.parse( localStorage.getItem('creepypastasCategorias') || '{}' );
+    this.creepypastasCategoriasKV = this.objTo2dArray(this.creepypastasCategoriasMap);
     http.get('https://public-api.wordpress.com/rest/v1/sites/creepypastas.com/categories')
       .map(res => res.json())
       .subscribe(response => {
-          this.creepypastasCategorias = response.categories.filter((item) => {
+          response.categories.filter((item) => {
             switch (item.ID) {
               case 464:
               case 396:
               case 185:
                 return false;
               default:
+                this.creepypastasCategoriasMap[item.ID] = item;
                 return true;
             }
           })
+          localStorage.setItem('creepypastasCategorias', JSON.stringify(this.creepypastasCategoriasMap));
+          this.creepypastasCategoriasKV = this.objTo2dArray(this.creepypastasCategoriasMap);
         }
       );
+  }
+
+  objTo2dArray(obj){
+    let arr = [];
+    for (var id in obj) {
+      if (obj.hasOwnProperty(id)) {
+        arr.push([ id, this.creepypastasCategoriasMap[id] ]);
+      }
+    }
+    return arr;
   }
 
   requestFilteredCreepypastas(event, item) {
