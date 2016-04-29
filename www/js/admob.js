7a3d55e17274fc623a9bf40c5ca6e2fa145ff41a
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2016-04-28 19:02:33 -0500

    new build: Retiro banner inferior. Retiro pantalla completa

--- www/js/admob.js
+++ www/js/admob.js
@@ -23,15 +23,6 @@ if(( /(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent) )) {
 function initApp() {
     if (! AdMob ) { console.error( 'admob plugin not ready' ); return; }
 
-    AdMob.createBanner( {
-        adId: admobid.banner,
-        isTesting: false,
-        overlap: false,
-        offsetTopBar: false,
-        position: AdMob.AD_POSITION.BOTTOM_CENTER,
-        bgColor: 'black'
-    } );
-
     AdMob.prepareInterstitial({
         adId: admobid.interstitial,
         autoShow: true
